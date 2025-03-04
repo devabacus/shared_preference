340a5bab3d66aa6fdc7bfe15561eb6057a112f1b
mrfrolk <ekaterita20@gmail.com>
2025-03-04 19:00:21 +0500

    1

--- lib/provider/counter.dart
+++ lib/provider/counter.dart
@@ -2,28 +2,34 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 part 'counter.g.dart';
 
+// @riverpod
+// class Counter extends _$Counter {
+//   @override
+//   int build() {
+//     return 77;
+//   }
 
+//   void increment() {
+//     state++;
+//   }
 
+// }
 
 @riverpod
 class Counter extends _$Counter {
   @override
-  int build() {
-    return 77;
+  FutureOr<int> build() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getInt('counter') ?? 0;
   }
 
-  void increment() {
-    state++;
+  Future<void> increment() async {
+    final currentState = await future;
+    
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setInt('counter', currentState + 1);
+    state = AsyncData(currentState + 1);
   }
 
-}
 
-
-@riverpod
-class sharedPrefGetCounterValue extends _$sharedPrefGetCounterValue {
-  @override
-  FutureOr<int> build() {
-    return 0;
-  }
-  
 }
